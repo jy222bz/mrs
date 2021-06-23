79344a420c891eba63fa18ca16a1d81edee775da
Jacob Yousif <46903975+jy222bz@users.noreply.github.com>
2021-06-23 22:00:18 +0100

    add(hbs pages):

--- controllers/addAuthorController.js
+++ controllers/addAuthorController.js
@@ -16,7 +16,7 @@ const controller = {}
  */
 controller.get = async (req, res) => {
   try {
-    await res.render('add/add_author')
+    await res.render('add/add-author')
   } catch (error) {
     console.log(error)
   }
@@ -32,7 +32,7 @@ controller.get = async (req, res) => {
  */
 controller.post = async (req, res) => {
   try {
-    await res.render('add/add_author')
+    await res.render('add/add-author')
   } catch (error) {
     console.log(error)
   }
