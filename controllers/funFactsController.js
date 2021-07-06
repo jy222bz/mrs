ba281aae85fde0502fc361a3380ae6dc1e50e48a
Jacob Yousif <46903975+jy222bz@users.noreply.github.com>
2021-07-06 20:52:11 +0100

    fix(view):

--- controllers/funFactsController.js
+++ controllers/funFactsController.js
@@ -7,7 +7,6 @@ require('dotenv').config()
 const auth = require('../validators/authenticator')
 const searchController = {}
 
-
 /**
  * This method it responds to the GET request when
  * the user wans to search for a specific tag.
@@ -16,7 +15,7 @@ const searchController = {}
  * @param {object} res the Express response.
  */
 searchController.get = async (req, res) => {
-  searchController.aggregateCheapest(req, res)
+  searchController.getTopPicturesView(req, res)
 }
 
 /**
@@ -26,211 +25,27 @@ searchController.get = async (req, res) => {
  * @param {object} req the Express request.
  * @param {object} res the Express response.
  */
-searchController.getMutual = async (req, res) => {
-  if (auth.checkAuthenticated(req)) {
-    try {
-      /**
-       * Exporting the DB connection.
-       *
-       * @param {object} req the Express request.
-       * @param {object} res the Express response.
-       */
-
-      db.getConnection((error, connection) => {
-        if (error) {
-          console.log(error)
-          process.exit(1)
-        } else {
-          console.log('MySQL is connected. Connection ID: ' + connection.threadId)
-        }
-        connection.query('SELECT DISTINCT d.fullName, d.age, d.origin, m.name AS name1, s.name AS name2 FROM directors_table d, movies_table m, series_table s WHERE d.fullName = m.director AND d.fullName = s.director', (err, rows) => {
-          connection.release()
-          if (!err) {
-            res.render('search/search', { mutual: rows })
+searchController.getTopPicturesView = async (req, res) => {
+  try {
+    db.getConnection((error, connection) => {
+      if (error) {
+        console.log(error)
+        process.exit(1)
+      }
+      connection.query('SELECT * FROM top_films_view', (err, rows) => {
+        connection.release()
+        if (!err) {
+          if (auth.checkAuthenticated(req)) {
+            res.render('extra/funfacts1', { top: rows })
+          } else {
+            res.render('extra/funfacts2', { top: rows })
           }
-        })
+        }
       })
-    } catch (error) {
-      console.log(error)
-    }
-  } else {
-    return res.redirect('/login')
+    })
+  } catch (error) {
+    console.log(error)
   }
-}
-
-/**
- * This method it responds to the GET request when
- * the user wans to search for a specific tag.
- *
- * @param {object} req the Express request.
- * @param {object} res the Express response.
- */
-searchController.crossJoins = async (req, res) => {
-  if (auth.checkAuthenticated(req)) {
-    try {
-      /**
-       * Exporting the DB connection.
-       *
-       * @param {object} req the Express request.
-       * @param {object} res the Express response.
-       */
-
-      db.getConnection((error, connection) => {
-        if (error) {
-          console.log(error)
-          process.exit(1)
-        }
-        connection.query('SELECT m.name AS name1, s.name AS name2, m.price AS price1, s.price AS price2, (m.price + s.price) AS total FROM movies_table m CROSS JOIN series_table s', (err, rows) => {
-          connection.release()
-          if (!err) {
-            res.render('search/search', { joins: rows, title: 'Search' })
-          }
-        })
-      })
-    } catch (error) {
-      console.log(error)
-    }
-  } else {
-    return res.redirect('/login')
-  }
-}
-
-/**
- * This method it responds to the GET request when
- * the user wans to search for a specific tag.
- *
- * @param {object} req the Express request.
- * @param {object} res the Express response.
- */
-searchController.innerJoins = async (req, res) => {
-  if (auth.checkAuthenticated(req)) {
-    try {
-      /**
-       * Exporting the DB connection.
-       *
-       * @param {object} req the Express request.
-       * @param {object} res the Express response.
-       */
-
-      db.getConnection((error, connection) => {
-        if (error) {
-          console.log(error)
-          process.exit(1)
-        } else {
-          console.log('MySQL is connected. Connection ID: ' + connection.threadId)
-        }
-        connection.query('SELECT m.name, m.director, m.category, b.rating FROM movies m INNER JOIN reviews b ON m.name = b.name', (err, rows) => {
-          connection.release()
-          if (!err) {
-            console.log(rows)
-            res.render('search/search', { innerJoins: rows })
-          }
-        })
-      })
-    } catch (error) {
-      console.log(error)
-    }
-  } else {
-    return res.redirect('/login')
-  }
-}
-
-/**
- * This method it responds to the GET request when
- * the user wans to search for a specific tag.
- *
- * @param {object} req the Express request.
- * @param {object} res the Express response.
- */
-searchController.innerJoins = async (req, res) => {
-  if (auth.checkAuthenticated(req)) {
-    try {
-
-      /**
-       * Exporting the DB connection.
-       *
-       * @param {object} req the Express request.
-       * @param {object} res the Express response.
-       */
-
-      db.getConnection((error, connection) => {
-        if (error) {
-          console.log(error)
-          process.exit(1)
-        } else {
-          console.log('MySQL is connected. Connection ID: ' + connection.threadId)
-        }
-        connection.query('SELECT m.name, m.director, m.category, b.rating, b.gross, b.grossWorldwide FROM movies_table m INNER JOIN box_office_table b ON m.name = b.name', (err, rows) => {
-          connection.release()
-          if (!err) {
-            console.log(rows)
-            res.render('search/search', { innerJoins: rows })
-          }
-        })
-      })
-    } catch (error) {
-      console.log(error)
-    }
-  } else {
-    return res.redirect('/login')
-  }
-}
-
-/**
- * This method it responds to the GET request when
- * the user wans to search for a specific tag.
- *
- * @param {object} req the Express request.
- * @param {object} res the Express response.
- */
-searchController.aggregateExpensive = async (req, res) => {
-  if (auth.checkAuthenticated(req)) {
-    try {
-      db.getConnection((error, connection) => {
-        if (error) {
-          console.log(error)
-          process.exit(1)
-        }
-        connection.query('SELECT * MAX(m.price) AS mPrice, m.name AS mName, s.name AS sName, MAX(s.price) AS sPrice FROM movies_table m, series_table s ORDER BY m.name', (err, rows) => {
-          connection.release()
-          if (!err) {
-            res.render('search/search', { expensive: rows })
-          }
-        })
-      })
-    } catch (error) {
-      console.log(error)
-    }
-  } else {
-    return res.redirect('/login')
-  }
-}
-
-/**
- * This method it responds to the GET request when
- * the user wans to search for a specific tag.
- *
- * @param {object} req the Express request.
- * @param {object} res the Express response.
- */
-searchController.aggregateCheapest = async (req, res) => {
-
-    try {
-      db.getConnection((error, connection) => {
-        if (error) {
-          console.log(error)
-          process.exit(1)
-        }
-        connection.query('SELECT r.movieID, r.rating AS rate FROM reviews r WHERE r.rating = (SELECT MAX(rating) FROM reviews)', (err, rows) => {
-          connection.release()
-          if (!err) {
-            console.log(rows)
-          }
-        })
-      })
-    } catch (error) {
-      console.log(error)
-    }
 }
 
 module.exports = searchController
--- routes/funFactsRouter.js
+++ routes/funFactsRouter.js
@@ -14,10 +14,5 @@ const controller = require('../controllers/funFactsController')
  */
 router
   .get('/fun-facts', controller.get)
-  .get('/mutual-directors', controller.getMutual)
-  .get('/cross-join', controller.crossJoins)
-  .get('/inner-join', controller.innerJoins)
-  .get('/aggregate-cheapest', controller.aggregateCheapest)
-  .get('/aggregate-expensive', controller.aggregateExpensive)
 
 module.exports = router
